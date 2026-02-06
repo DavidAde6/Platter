f3556ded4d794771b8a4abff112cfe75a7516636
David Adeniyi <dta123adeniyi@gmail.com>
2026-02-06 02:55:23 -0500

    Designed basic UI

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 
 #IMPORTS
-from fastapi import FastAPI, File, UploadFile, HTTPException
-#from transformers import pipeline
-from pydantic import BaseModel # data validation & parsing
+from fastapi import FastAPI, Request, File, UploadFile, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import requests
 from PIL import Image # IMAGE HANDLING
 import io
 from dotenv import load_dotenv
 import os
 import requests
 from ultralytics import YOLO
+
 
 # TO RUN SERVER type - uvicorn main:app --reload
 # go to http://127.0.0.1:8000/docs
@@ -23,20 +25,37 @@ app = FastAPI()
 NUTRIENT_ID = [1008, 1005, 1003, 1079, 2000, 1004, 1257, 1258, 1292, 1293]
 NUTRIENT_NAME = ['Energy', 'Carbohydrate', 'Protein', 'Fiber', 'Sugars', 'Total Fat', 'Trans Fat', 'Saturated fats', 'Monosaturated fats', 'Polysaturated fats']
 NUTRIENT_MAP = dict(zip(NUTRIENT_ID, NUTRIENT_NAME))
+templates = Jinja2Templates(directory="templates")
 
 model = YOLO("last.pt")
+rows = []
 
 # ---------------------FUNCTIONS--------------------------------------------
 
 
 
 
+#---------------------GET REQUESTS---------------------------------------
+@app.get("/", response_class=HTMLResponse)
+async def show_form(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "rows": None}
+    )
 
 # ---------------------POST REQUESTS----------------------------------------
 
+# @app.post("/", response_class=HTMLResponse)
+# async def handle_form(request: Request, name: str = Form(...)):
+#     rows = search_usda(name)
+#     return templates.TemplateResponse(
+#         "index.html",
+#         {"request": request, "rows": rows, "query": name}
+#     )
+
 # Getting image------------------------------------------------------------
-@app.post("/upload/")
-async def upload_img(file: UploadFile = File(...)):
+@app.post("/", response_class=HTMLResponse)
+async def upload_img(request: Request,file: UploadFile = File(...)):
 
     if not file.content_type.startswith("image/"): # if not an image
         raise HTTPException(status_code=400, detail="File is not an Image") # temporary stop
@@ -56,7 +75,7 @@ async def upload_img(file: UploadFile = File(...)):
     # stores values
     top_idx = int(r.probs.top1)
     top_conf = float(r.probs.top1conf)
-    class_name = r.names[top_idx] # the name u need
+    class_name = r.names[top_idx].replace("_", " ") # the name u need
 
     #USDA QUERY USING NAME!
     url = "https://api.nal.usda.gov/fdc/v1/foods/search"
@@ -69,17 +88,18 @@ async def upload_img(file: UploadFile = File(...)):
     # gets json data
     r = requests.get(url, params=params).json()
     foods = r["foods"]
-
+    chosen_food = foods[0]
     # returns one with
     for food in foods:
         ids = {n["nutrientId"] for n in food.get("foodNutrients", []) if n.get("amount") is not None}
         if all(nid in ids for nid in NUTRIENT_ID):  # Has ALL needed
-            return food
+            chosen_food = food
+            break
     
     macros = {}
 
     #gets nutrient info
-    for i, n in enumerate(food.get("foodNutrients", [])):
+    for i, n in enumerate(chosen_food.get("foodNutrients", [])):
         if n["nutrientId"] in NUTRIENT_ID:
             name = NUTRIENT_MAP[n["nutrientId"]]
             macros[name] = {
@@ -87,11 +107,16 @@ async def upload_img(file: UploadFile = File(...)):
                 "unit": n.get("unitName", "")
             }
     
-
-    return {
-        "Food": food["description"],
-        "fdcId": food["fdcId"],
-        "Serving": f"{food.get('servingSize', 100)}g",
+    row = {
+        "Food": chosen_food["description"],
+        "fdcId": chosen_food["fdcId"],
+        "Serving": f"{chosen_food.get('servingSize', 100)}g",
         "macros": macros
     }
+
+    rows.append(row)
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "rows": rows, "query": class_name}
+    )
 
